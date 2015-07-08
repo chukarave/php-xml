306d48283b9887879f3c71b37b08191dd8b68667
Noa Rave <noa.rave@berlinonline.de>
2015-07-08 17:01:54 +0200

    add date attribute

--- rss.php
+++ rss.php
@@ -17,13 +17,22 @@ $app->get("/", function() use ($app){
     $rss_url ='http://feeds.feedburner.com/EatingwellBlogs-AllBlogPosts?format=xml';
 
     $xml = simplexml_load_file($rss_url);
+    $feed = $xml->entry;
+    echo $feed;
+    return $app['twig']->render('main.twig', array ('feed' => $feed);
+});
+
+/*
+$app->get("/", function() use ($app){
+    $rss_url ='http://feeds.feedburner.com/EatingwellBlogs-AllBlogPosts?format=xml';
+
+    $xml = simplexml_load_file($rss_url);
     $feed = $xml->channel->item;
     $desc = strip_tags($feed->description, '<a></a>');
 
     return $app['twig']->render('main.twig', array ('feed' => $feed, 'desc' => $desc));
 });
-
-
+ */
 $app->run();
 
 
