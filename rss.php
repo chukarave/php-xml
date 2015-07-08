e09097fba635e8b604404f5c5bcc5acb57b7282d
Noa Rave <noa.rave@berlinonline.de>
2015-07-08 11:11:23 +0200

    first commit

---
+++ rss.php
@@ -0,0 +1,31 @@
+<?php
+
+require "vendor/autoload.php";
+
+use Silex\Application;
+use Silex\Provider\TwigServiceProvider;
+
+$app = new Silex\Application();
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+$app->register(new TwigServiceProvider(), [
+    'twig.path' => __DIR__ . '/views' // path to Twig templates.
+]);
+
+
+$app->get("/", function() use ($app){
+    $rss_url ='http://feeds.feedburner.com/EatingwellBlogs-AllBlogPosts?format=xml';
+
+    $xml = simplexml_load_file($rss_url);
+    
+    $feed = $xml->channel->item;
+
+    return $app['twig']->render('main.twig', array ('feed' => $feed));
+});
+
+
+$app->run();
+
+
+
+
